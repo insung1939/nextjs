4691eb3713b9c1f9f152577a019e62709f751998
조인성 <insung@joinseong-ui-MacBookPro.local>
2022-04-10 13:52:38 +0900

    redirect and rewrites

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Seo from "../components/Seo"
+
 export default function About() {
     return (
         <div>
+            <Seo title="about" />
             <h1>About</h1>
         </div>
     )
